0c86ffcdcd428887313d20d132a5c335f82a13be
Vinicius Moraes <vsmoraes@vsmoraes.com>
2026-01-03 20:36:04 +0100

    Add conform to autoformat files

---
+++ lua/vsmoraes/lazy/conform.lua
@@ -0,0 +1,28 @@
+return {
+  'stevearc/conform.nvim',
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local conform = require("conform")
+
+    conform.setup({
+      formatters_by_ft = {
+        kotlin = { "ktlint" },
+        lua = { "stylua" },
+      },
+      format_on_save = {
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 1000,
+      },
+    })
+
+    -- Optional: Manual format keybinding
+    vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+      conform.format({
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 1000,
+      })
+    end, { desc = "Format file or range (in visual mode)" })
+  end,
+}
