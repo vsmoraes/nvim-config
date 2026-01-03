14a2b06f3d8caa20ba36477a1f6838e74dd949f8
Vinicius Moraes <vsmoraes@vsmoraes.com>
2026-01-03 15:13:57 +0100

    Fix wrong indentation on some files

--- lua/vsmoraes/lazy/colors.lua
+++ lua/vsmoraes/lazy/colors.lua
@@ -1,10 +1,10 @@
-return { 
-	{ 
-		"Mofiqul/dracula.nvim",
-		lazy = false,
-		opts = {},
-		config = function()
-			vim.cmd.colorscheme("dracula")
-		end
-	},
+return {
+    {
+        "Mofiqul/dracula.nvim",
+        lazy = false,
+        opts = {},
+        config = function()
+            vim.cmd.colorscheme("dracula")
+        end
+    },
 }
